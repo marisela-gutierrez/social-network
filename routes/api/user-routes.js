70e9178e41abe898ae762d42a867be09a1ea0833
marigut <mari.gutierrez96@yahoo.com>
2021-08-07 22:44:56 -0500

    user and thought routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller');
+
+// /api/Users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/Users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+  router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
